9059d360ddcb65686191a3db84ed8e310fcba93a
aquan <qaoimu@163.com>
2016-07-07 22:23:01 +0800

    frequency set and asocciation rules

--- preProcess.py
+++ preProcess.py
@@ -2,12 +2,24 @@
 
 import sys
 fpIn = open("diagnosis.data","r")
+fpOut = open("preDiagnosis.data","wt")
 attr = ['a1','a2','a3','a4','a5','a6','d1','d2']
 
+
 lines = fpIn.readlines()
-
+linesCount = len(lines)
 for line in lines:
-    para = line.split("	")
+    para = line.split('\t')
+    para0 = line.split(',')
+    fpOut.write(para0[0])
+    i = 0
     for p in para:
-        print p
+        if i > 0:
+            if p == 'yes':
+                fpOut.write(',')
+                fpOut.write(attr[i])
+        i += 1
+    fpOut.write('\n')
 
+fpIn.close()
+fpOut.close()

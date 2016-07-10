15cd97951a20e540b350ead6ded8b623c1d18605
aoquan <qaoimu@163.com>
2016-07-10 12:13:30 +0800

    done

--- preProcess.py
+++ preProcess.py
@@ -3,21 +3,26 @@
 import sys
 fpIn = open("diagnosis.data","r")
 fpOut = open("preDiagnosis.data","wt")
-attr = ['a1','a2','a3','a4','a5','a6','d1','d2']
+attr1 = ['a2_no','a3_no','a4_no','a5_no','a6_no','d1_no','d2_no']
+attr2 = ['a2_yes','a3_yes','a4_yes','a5_yes','a6_yes','d1_yes','d2_yes']
 
 
 lines = fpIn.readlines()
 linesCount = len(lines)
 for line in lines:
+    line = line.strip('\n')
     para = line.split('\t')
     para0 = line.split(',')
-    fpOut.write(para0[0])
+    fpOut.write('a1_'+para0[0])
     i = 0
     for p in para:
         if i > 0:
             if p == 'yes':
                 fpOut.write(',')
-                fpOut.write(attr[i])
+                fpOut.write(attr2[i-1])
+            else:
+                fpOut.write(',')
+                fpOut.write(attr1[i-1])
         i += 1
     fpOut.write('\n')
 
---
+++ run.R
@@ -0,0 +1,52 @@
+#设置工作路径
+setwd('/home/aoquan/git/DMhomework2/')
+
+#加载算法库
+library("Matrix")
+library("arules")
+library("arulesViz")
+
+#读取数据，以Basket方式
+#tr<-read.transactions("traindata.csv",format="basket",sep=",",rm.duplicates=TRUE)
+tr<-read.transactions("preDiagnosis.data",format="basket",sep=",",rm.duplicates=TRUE)
+
+#获得频繁项集
+frequentsets=eclat(tr,parameter=list(support=0.3,maxlen=4))
+summary(frequentsets)
+inspect(frequentsets)  
+#查看支持度最高的前20个频繁项集
+inspect(sort(frequentsets,by="support")[1:20]) 
+
+#抽取关联规则
+rules = apriori(tr,parameter = list(support = 0.3,confidence = 0.3))
+summary(rules) 
+inspect(rules)
+
+
+# #筛选右变元为幸存的规则子集
+# x=subset(rules,subset=rhs%in%"Survive")     
+# x
+# inspect(x)
+# 
+# #筛选右变元为死亡的规则子集
+# x=subset(rules,subset=rhs%in%"Dead")
+# x
+# inspect(x)
+
+#根据支持度对求得的关联规则子集排序并察看
+inspect(sort(rules,by="support")[1:10])  
+#根据置信度对求得的关联规则子集排序并察看
+inspect(sort(rules,by="confidence")[1:10]) 
+
+#根据lift对求得的关联规则子集排序并察看
+inspect(sort(rules,by="lift")[1:10])   
+itemFrequencyPlot(tr,support = 0.3,cex.names =0.8) 
+
+
+#plot(rules)
+
+#plot(rules, measure = c("support", "lift"), shading = "confidence")
+#画泡泡图
+#plot(rules, method = "grouped")
+#画平行坐标图
+plot(rules, method="paracoord", control=list(reorder=TRUE))
